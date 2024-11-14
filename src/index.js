e3af98772bd9cb66de35bd0b9708b86fb5af9990
aminebichiou <aminebichiou2@gmail.com>
2024-11-14 01:32:09 +0100

    deploying

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
+import reportWebVitals from "./reportWebVitals";
 
 const Main = () => {
   useEffect(() => {
+    // Function to manage theme changes
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Get the saved theme or fallback to system preference
     const savedTheme =
       localStorage.getItem("theme") ||
       (prefersDarkScheme.matches ? "dark-mode" : "light-mode");
+
+    // Apply the saved theme
     document.body.classList.add(savedTheme);
-    prefersDarkScheme.addEventListener("change", (e) => {
+
+    // Add listener to handle theme change when system preference changes
+    const handleThemeChange = (e) => {
       document.body.classList.toggle("dark-mode", e.matches);
       document.body.classList.toggle("light-mode", !e.matches);
-    });
-  }, []);
+    };
+
+    prefersDarkScheme.addEventListener("change", handleThemeChange);
+
+    // Cleanup function to remove event listener when component unmounts
+    return () => {
+      prefersDarkScheme.removeEventListener("change", handleThemeChange);
+    };
+  }, []); // Empty dependency array ensures the effect runs once on mount
 
   return <App />;
 };
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
+
+reportWebVitals();
